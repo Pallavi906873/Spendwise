0efee6212e987d6bf4e6f44f91028c5032ffed9e
pallavi <pallavi.a@thaariva.ai>
2026-08-28 09:21:30 +0530

    Fix frontend Jest configuration

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest'
+    '^.+\\.(js|jsx)$': ['babel-jest', {
+      presets: ['@babel/preset-env', '@babel/preset-react']
+    }]
   }
 }
